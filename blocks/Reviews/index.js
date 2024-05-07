515e018133e1e1ef59a19b2d551e3bc40a391d57
Joel Corey <hello@joelcorey.com>
2024-05-06 19:44:54 -0700

    testing again

--- blocks/Reviews/index.js
+++ blocks/Reviews/index.js
@@ -46,7 +46,7 @@ const reviewStars = (starNumber) => {
 			xmlns="http://www.w3.org/2000/svg">
 			<path
 					d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-			</svg>
+			</svg>d
 		)
 		remainder -= 1
 	}
