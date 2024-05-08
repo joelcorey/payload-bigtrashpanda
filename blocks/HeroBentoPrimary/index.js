3c6fca84526fd88fec44787b67a05edd9f9fadd9
Joel Corey <hello@joelcorey.com>
2024-05-08 11:57:26 -0700

    testing gh actions

--- blocks/HeroBentoPrimary/index.js
+++ blocks/HeroBentoPrimary/index.js
@@ -10,6 +10,8 @@ const borders = (showBorders) => {
 	return showBorders ? 'border border-1 border-black' : ''
 }
 
+// testing github actions
+
 let slides = [
 	'/img/component/carousel/carousel-1.svg',
 	'/img/component/carousel/carousel-2.svg',
