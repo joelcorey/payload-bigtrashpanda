b756902d89801a7313763ebcad4498d9294c27b1
Joel Corey <hello@joelcorey.com>
2024-08-15 05:11:56 -0700

    bump versions, partial fixes

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,10 +1,10 @@
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { s3Storage } from '@payloadcms/storage-s3'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
+import { 
+	lexicalEditor 
+} from '@payloadcms/richtext-lexical'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { buildConfig } from 'payload'
 import sharp from 'sharp'
-import { buildConfig } from 'payload/config'
 import { fileURLToPath } from 'url'
 
 import { Users } from './collections/Users'
@@ -12,6 +12,29 @@ import { Media } from './collections/Media'
 import Page from './collections/Page';
 import Category from './collections/Category';
 import Color from './collections/Color'
+
+// The new way of doing it that does not work??
+//https://github.com/payloadcms/payload/tree/beta/packages/storage-s3
+import { s3Storage } from '@payloadcms/storage-s3'
+
+// The old way
+// import { cloudStorage } from '@payloadcms/plugin-cloud-storage'
+// import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3'
+// const adapter = s3Adapter({
+//   config: {
+// 		endpoint: process.env.S3_ENDPOINT,
+//     credentials: {
+// 			//@ts-ignore
+//       accessKeyId: process.env.S3_ACCESS_KEY_ID,
+// 			//@ts-ignore
+//       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+//     },
+//     region: process.env.S3_REGION,
+//     // ... Other S3 configuration
+//   },
+// 	//@ts-ignore
+//   bucket: process.env.S3_BUCKET,
+// })
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -36,29 +59,37 @@ export default buildConfig({
     url: process.env.MONGODB_URI || '',
   }),
 
-	// db: postgresAdapter({
-  //   pool: {
-  //     connectionString: process.env.POSTGRES_URI,
-  //   },
-  // }),
-
   sharp,
 
 	plugins: [
+	  //@ts-ignore
     s3Storage({
       collections: {
-        Media,
+				media: true
       },
+			//@ts-ignore
       bucket: process.env.S3_BUCKET,
       config: {
-				forcePathStyle: true,
-        credentials: {
+				credentials: {
+					//@ts-ignore
           accessKeyId: process.env.S3_ACCESS_KEY_ID,
+					//@ts-ignore
           secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+					endpoint: process.env.S3_ENDPOINT,
+					region: process.env.S3_REGION,
         },
-        region: process.env.S3_REGION,
-        endpoint: process.env.S3_ENDPOINT
+        // ... Other S3 configuration
       },
     }),
   ],
+
+	// plugins: [
+	// 	cloudStorage({
+	// 		collections: {
+	// 			'media': {
+	// 				adapter
+	// 			},
+	// 		},
+	// 	}),
+	// ]
 })
